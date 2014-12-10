34bb489e3e36e6e56b1d6b6c434e32bb551b53b8
inkJake <inkjake@gmail.com>
2014-12-11 00:43:02 +0800

    remove 3rd part session class.

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -52,7 +52,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('database','session','MY_Session','My_tools');
+$autoload['libraries'] = array('database','session','My_tools');
 
 
 /*
